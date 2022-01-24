69cd1936077b1a16063d3f8ea5f0fc8550062af4
Marie2866 <94830782+Marie2866@users.noreply.github.com>
2022-01-24 13:55:02 +0100

    Update index.js : add fonction mongodb

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require("express");
+//const express = require("express");
+const mongodb = require("mongodb");
 
-const app = express();
-app.use(express.json());
+//const app = express();
+//app.use(express.json());
+const app = mongodb();
+app.use(mongodb());
 
 // CREATE users
 app.post("/users", (req, res) => {
